52f83940c952b93c4738b1f298f2536756a6d7b4
Alberto Franzin <albertofranzin@users.noreply.github.com>
2013-03-16 12:33:02 +0000

    sistemata una cazzata

--- source/ds/utils.h
+++ source/ds/utils.h
@@ -32,10 +32,7 @@ extern double incumbent;
 #define distance(x1, y1, x2, y2) \
 	sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
 
-void init();
-
 void initializeRandom(long seed);
-void initializeRandomWithTime();
 
 short parHash(char *);
 parameters *getParameters();
