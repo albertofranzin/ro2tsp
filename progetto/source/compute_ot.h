72d0942317c639a61ecd18a1f41bc74e0e103fa9
alberto.franzin@gmail.com <alberto.franzin@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-03-26 10:31:28 +0000

    aggiunti make e config ; fuso struttura cartelle e aggiornamenti

--- progetto/source/compute_ot.h
+++ progetto/source/compute_ot.h
@@ -1,7 +1,8 @@
-#include "graph.h"
-
 #ifndef COMPUTE_OT_H_
 #define COMPUTE_OT_H_
+
+#include "graph.h"
+#include "compute_mst.h"
 
 void compute_ot(graph* G, graph* T);
 
--- progetto/source/solve_tsp.h
+++ progetto/source/solve_tsp.h
@@ -1,7 +1,12 @@
-#include "graph.h"
-
 #ifndef SOLVE_TSP_H_
 #define SOLVE_TSP_H_
+
+#include <stdlib.h>
+#include "graph.h"
+#include "utils.h"
+
+#define BIG 1000
+#define SMALL -1000
 
 void solve_tsp(graph* G, graph* H, double* incumbent, int flag);
 int is_cycle(graph* G);
