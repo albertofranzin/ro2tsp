29bc9a159cc1169339ee534c73aa5278b833e438
Ludovico Minto <ludovico.minto@gmail.com>
2013-03-26 22:08:42 +0000

    aggiunti commenti

--- progetto/source/compute_ot.h
+++ progetto/source/compute_ot.h
@@ -4,6 +4,11 @@
 #include "graph.h"
 #include "compute_mst.h"
 
+/* calcola l'1-albero di G e salva il risultato in T;
+ * - si assume G, T grafi inizializzati;
+ * - si assume G grafo di n > 2 nodi, non diretto, completo;
+ * - il contenuto iniziale di G viene eliminato;
+ */
 void compute_ot(graph* G, graph* T);
 
 #endif
--- progetto/source/node.h
+++ progetto/source/node.h
@@ -1,10 +1,12 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+/* nodo di un grafo;
+ */
 struct node {
-  double x;
-  double y;
-  int deg;
+  double x; // coordinata asse x;
+  double y; // coordinata asse y;
+  int deg; // grado del nodo (numero di lati incidenti nel nodo);
 };
 
 typedef struct node node;
