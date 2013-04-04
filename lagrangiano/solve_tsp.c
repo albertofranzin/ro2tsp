e731b05fb21f7c6da4d77271a9193c16b46a098b
ludovico.minto@gmail.com <ludovico.minto@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-04-04 12:05:41 +0000

    aggiornata cartella lagrangiano

---
+++ lagrangiano/solve_tsp.c
@@ -0,0 +1,162 @@
+#include "graph.h"
+#include "solve_tsp.h"
+
+graph INITIAL_GRAPH;
+
+void solve_tsp(graph* G, graph* H, double* incumbent, int flag) {
+  int i, j, u, v, w;
+  double z;
+  graph ONE_TREE;
+  int n = (*G).n;
+  double* previous_cost;;
+  double cost_wv, cost_wu;
+
+  /* effettua una copia del grafo iniziale G, passato in ingresso alla prima chiamata della funzione;
+   */
+  if (flag == 0) {
+    initGraph(&INITIAL_GRAPH, 1);
+    copyGraph(G, &INITIAL_GRAPH);
+    flag = 1;
+  }
+  else {
+    flag = 2;
+  }
+
+  initGraph(&ONE_TREE, 1);
+
+  /* calcola 1-albero;
+   */
+  compute_ot(G, &ONE_TREE);
+
+  /* calcola z = costo dell'1-albero;
+   */
+  for (i = 1; i <= n; i++) {
+    for (j = i+1; j <= n; j++)
+      set_edge_cost(&ONE_TREE, i, j, get_edge_cost(&INITIAL_GRAPH, i, j));
+  }
+  z = get_graph_cost(&ONE_TREE);
+
+  /* verifica se possibile potare il ramo corrente
+   */
+  if (z >= *incumbent) {
+    deleteGraph(&ONE_TREE);
+    return;
+  }
+
+  /* verifica se è stato trovato un ciclo (una soluzione ottima per il sottoproblema corrispondente al nodo corrente);
+   * aggiorna incumbent e soluzione ottima corrente;
+   */
+  if (is_cycle(&ONE_TREE)) {
+    printf("updating incumbent : from %f to %f\n", *incumbent, z);
+    if (z < *incumbent) {
+      *incumbent = z;
+      copyGraph(&ONE_TREE, H);
+    }
+    deleteGraph(&ONE_TREE);
+    return;
+  }
+
+  /* cerca un nodo w con almeno 3 lati incidenti;
+   */
+  for (w = 1; w <= n; w++) {
+    if (get_node_deg(&ONE_TREE, w) >= 3)
+      break;
+  }
+
+  /* (tentativo di) ricerca di due lati candidati mai forzati nè vietati;
+   */
+  for (v = 1; v <= n; v++) {
+   if (v != w && adjacent(&ONE_TREE, w, v) && get_edge_cost(G, w, v) > SMALL && get_edge_cost(G, w, v) < BIG)
+      break;
+  }
+  for (u = 1; u <= n; u++) {
+    if (u != w && u != v && adjacent(&ONE_TREE, w, u) && get_edge_cost(G, w, u) > SMALL && get_edge_cost(G, w, u) < BIG)
+      break;
+  }
+
+  deleteGraph(&ONE_TREE);
+
+  /* se esistono due lati mai selezionati allora procedi con BRANCHING A 3 VIE;
+   */
+  if ((v >= 1 && v <= n) && (u >= 1 && u <= n)) {
+    
+    // vieta il lato {w, v};
+    cost_wv = get_edge_cost(G, w, v);
+    set_edge_cost(G, w, v, BIG);
+    solve_tsp(G, H, incumbent, flag);
+    set_edge_cost(G, w, v, cost_wv);
+
+    // forza il lato {w, v} e vieta il lato {w, u};
+    cost_wv = get_edge_cost(G, w, v);
+    cost_wu = get_edge_cost(G, w, u);
+    set_edge_cost(G, w, v, SMALL);
+    set_edge_cost(G, w, u, BIG);
+    solve_tsp(G, H, incumbent, flag);
+    set_edge_cost(G, w, v, cost_wv);
+    set_edge_cost(G, w, u, cost_wu);
+
+    // forza i lati {w, v} e {w, u}, vieta tutti gli altri lati;
+    previous_cost = (double*)malloc(sizeof(double) * n);
+    for (i = 1; i <= n; i++) { // memorizza i costi attuali dei lati per poter fare un roll back al ritorno dalla prossim chiamata ricorsiva;
+      if (i != w) 
+	previous_cost[i-1] = get_edge_cost(G, w, i);
+    }
+    set_edge_cost(G, w, v, SMALL);
+    set_edge_cost(G, w, u, SMALL);
+    for (i = 1; i <= n; i++) {
+      if (i != w && i != v && i != u)
+	set_edge_cost(G, w, i, BIG);
+    }
+  solve_tsp(G, H, incumbent, flag);
+    for (i = 1; i <= n; i++) { // roll back dei costi così com'erano prima della chiamata ricorsiva;
+      if (i != w)
+	set_edge_cost(G, w, i, previous_cost[i-1]);
+    }
+    free(previous_cost);
+  } 
+
+  /* esiste un solo lato che non è mai stato nè forzato nè vietato;
+   */
+  else if (((v >= 1 && v <= n) && (u < 1 || u > n)) || ((v < 1 || v > n) && (u >= 1 && u <= n))) {
+    
+    // vieta il lato {w, v};
+    cost_wv = get_edge_cost(G, w, v);
+    set_edge_cost(G, w, v, BIG);
+    solve_tsp(G, H, incumbent, flag);
+    set_edge_cost(G, w, v, cost_wv);
+
+    // forza il lato {w, u};
+    cost_wv = get_edge_cost(G, w, v);
+    set_edge_cost(G, w, v, SMALL);
+    solve_tsp(G, H, incumbent, flag);
+    set_edge_cost(G, w, v, cost_wv);
+  }
+
+  /* tutti i lati sono già stati forzati o vietati;
+   */
+  else if ((v < 1 || v > n) && (u < 1 || u > n)) {
+    return;
+  }
+
+  /* copia il grafo iniziale nuovamente il G (la funzione in questo modo non modifica gli oggetti passati in ingresso);
+   */
+  if (flag == 1) {
+    copyGraph(&INITIAL_GRAPH, G);
+    deleteGraph(&INITIAL_GRAPH);
+    flag = 2;
+  }
+}
+
+/* ritorna 1 se G è un ciclo, 0 altrimenti;
+ * - si assume G connesso (altrimenti non è detto che G sia un ciclo anche se la funzione ritorna 1;
+ */
+int is_cycle(graph* G) {
+  int i;
+  int n = (*G).n;
+ 
+  for (i = 1; i <= n; i++) {
+    if (get_node_deg(G, i) != 2)
+      return 0;
+  }
+  return 1;
+}
---
+++ lagrangiano/utils.c
@@ -0,0 +1,164 @@
+#include "utils.h"
+
+inline int atPosition(int i, int j) {
+	if (i > j) {
+		return (i * (i-1)) / 2 + j;
+	} else {
+		return (j * (j-1)) / 2 + i;
+	}
+}
+
+unsigned long initializeRandom(unsigned int seed) {
+	unsigned long sd = seed;
+	if (sd == 0) {
+		time_t now = time ( 0 ); 
+		unsigned char *p = (unsigned char *)&now;
+		sd = 0;
+		size_t i;
+
+		for ( i = 0; i < sizeof now; i++ ) 
+			sd = sd * ( UCHAR_MAX + 2U ) + p[i];
+	}
+	srand(sd);
+	//printf("utils.c :: initializeRandom :: srand done\n");
+	return sd;
+}
+
+/*
+ * parHash
+ * parName : the string
+ *
+ * 'manual' hashing for the parameter name, to fill in the parameter list
+ * the values are hard-coded here
+ * Maybe ugly, but easy.
+ *
+ * return : an integer associated to the parameter
+ */
+short parHash(char *parName) {
+	if (strcmp(parName, "NUMBER_OF_NODES") == 0)
+		return 0;
+	if (strcmp(parName, "SEED") == 0)
+		return 1;
+	if (strcmp(parName, "PLOT") == 0)
+		return 2;
+	if (strcmp(parName, "PLOT_ONLY_1TREE") == 0)
+		return 3;
+	return -1;
+}
+
+// read parameters from config file
+// default values for parameters are set here
+parameters *getParameters() {
+	parameters *pars = malloc(sizeof(parameters));
+	//memset(pars, 0, sizeof(parameters));
+
+	pars->no_of_nodes = 10;
+	pars->seed = 0;
+	pars->plot = 1;
+	pars->plotOnlyTree = 0;
+
+	FILE *parFile = fopen(FILE_CONFIG, "r");
+	long i = 0, j;
+	char line[128];
+	char *p1, *p2;
+	int lineLen;
+
+	if ( parFile != NULL ) {
+		while( fgets(line, sizeof line, parFile) != NULL ){
+			lineLen = strlen(line)-1;
+
+			// skip empty lines
+			if(lineLen == 0) continue;
+			if(line[lineLen] == '\n') line[lineLen] = 0;
+
+			// skip comments
+			if(line[0] == '#') continue;
+
+			// split and strip away the spaces
+			// removing the spaces is necessary in order to use
+			// the parHash() method above
+			p1 = strtok(line, "= ");
+			p2 = strtok(NULL, "= ");
+
+			if(p2 != NULL) {
+				// manage correctly each parameter
+				switch(parHash(p1)) {
+					case 0 : pars->no_of_nodes = atoi(p2);
+							 break;
+					case 1 : pars->seed = atoi(p2);
+							 break;
+					case 2 : if (p2[0] == 'y' || p2[0] == 'Y')
+							 	pars->plot = 1;
+							 else
+							 	pars->plot = 0;
+							 break;
+					case 3 : if (p2[0] == 'y' || p2[0] == 'Y')
+							 	pars->plotOnlyTree = 1;
+							 else
+							 	pars->plotOnlyTree = 0;
+							 break;
+
+					default: break;
+				}
+			}
+		}
+		fclose(parFile);
+	} else {
+		/* errore nell'apertura del file */
+		perror( FILE_CONFIG );
+	}
+
+	return pars;
+}
+
+/*
+ * snbdComp
+ *const void * a,b : the two elements to be compared
+ *
+ * comparison function to be passed to qsort()
+ *
+ * return: -1,0,1 depending on the comparison of the two elements
+ */
+/*int snbdComp(const void *aa, const void *bb) {
+	node **a = (node **)aa,
+		 **b = (node **)bb;
+
+	if ((*a)->deg < (*b)->deg) { return -1; }
+	if ((*a)->deg > (*b)->deg) { return 1; }
+	return 0;
+}
+
+// sort edges by weight
+int sebwComp(const void *aa, const void *bb) {
+	edge **a = (edge **)aa,
+		 **b = (edge **)bb;
+
+	if ((*a)->weight < (*b)->weight) { return -1; }
+	if ((*a)->weight > (*b)->weight) { return 1; }
+	return 0;
+}*/
+
+/*void appendDouble(double **nnl, double n, int pos) {
+	double *nl = (double *)(*nnl);
+
+	double *tmp = realloc(nl, sizeof(nl) + sizeof(double)); 
+	if (tmp != NULL) {
+		memcpy(&(tmp), &(nl), sizeof(nl));
+		// beware : 'pos' correctness is not checked!
+		tmp[pos] = n;
+		nl = tmp;
+	} else {
+		fprintf(stderr, "memory allocation failed - smthg very close to a segfault [evil laugh] \n");
+		exit(1);
+	}
+}*/
+
+/*
+ * matching
+ * - G : the graph on which computing the matching
+ *
+ * compute maximal matching using the Hungarian algorithm
+ */
+double matching(graph *G) {
+	
+}
