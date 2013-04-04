ef0e031377d25d072c9fc9f84806ed0a62d08d8d
Ludovico Minto <ludovico.minto@gmail.com>
2013-04-04 11:58:04 +0000

    eliminazione lagrange.h e lagrange.c, sostituiti da compute_lagrange.h e compute_lagrange.c

--- lagrangiano/lagrange.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAGRANGE_H_
-#define LAGRANGE_H_
-
-#include "graph.h"
-
-double lagrange(graph* G, graph* H, int K, double ub, double alpha, int alpha_decading);
-double plot_lagrange(graph* G, graph* H, int K, double ub, double alpha, int alpha_decading);
-
-#endif
