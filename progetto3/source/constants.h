637dd4b7ca326d17123e503abe253e9eb8b5973b
ludovico.minto@gmail.com <ludovico.minto@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-04-10 13:56:49 +0000

    aggiunto constants.h

---
+++ progetto3/source/constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+// valori convenzionali per i costi +infinito e -infinito dei lati 
+#define BIG 2000
+#define SMALL -2000
+
+// dimensioni grafo gnuplot
+#define X_MIN -0.010
+#define X_MAX 1.010
+#define Y_MIN -0.010
+#define Y_MAX 1.010
+
+// parametri main
+#define DEFAULT_NUMBER_OF_NODES 10
+#define DEFAULT_SEED 0
+
+#endif
