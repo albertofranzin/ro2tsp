5044af808a19d5858234df9937fc60e7c3d859c5
ludovico.minto@gmail.com <ludovico.minto@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-04-11 09:52:01 +0000

    aggiunto lagrangiano in BeB

--- progetto3/source/test.c
+++ progetto3/source/test.c
@@ -45,26 +45,32 @@ void main(int argc, char** argv) {
   egraph_to_graph(&EG, &G);
 
   printf("@ Euclidean TSP\n# number of nodes = %d\n# seed = %d\n\n", n, s);
-
   double heuristic_upper_bound;
   heuristic_upper_bound = compute_upper_bound(&G);
-  printf("@ Nearest Neighbour Heuristic\n# upper bound = %f\n\n", heuristic_upper_bound); 
+  printf("@ Nearest Neighbour Heuristic\n# upper bound = %f\n", heuristic_upper_bound); 
 
+  /*
   double lagrangean_lower_bound;
   double ub = heuristic_upper_bound;
-  int max_num_of_iterations = 1000;
-  int slack_time = 100;
-  int alpha_halving_time = 10;
+  int max_num_of_iterations = 30000;
+  int slack_time = 20000;
+  int alpha_halving_time = 200;
   onetree HL;
   onetree_init(&HL, 1);
   printf("@ Lagrangean Dual Problem - Subgradient Algorithm\n");
   printf("# upper bound = %f\n# max number of iterations = %d\n# max number of iterations without improvements = %d\n# alpha = %f\n# alpha halving time = %d\n", ub, max_num_of_iterations, slack_time, ALPHA, alpha_halving_time);
-  lagrangean_lower_bound = compute_lagrange(&G, &HL, ub, max_num_of_iterations, slack_time, alpha_halving_time);
-
+  lagrangean_lower_bound = compute_lagrange(&G, &HL, ub);
+  egraph EG2;
+  egraph_init(&EG2, 1);
+  egraph_copy(&EG, &EG2);
+  onetree_to_egraph(&HL, &EG2);
+  egraph_plot(&EG, &EG2);
+  */
+ 
   double incumbent = heuristic_upper_bound + EPSILON;
   onetree H;
   onetree_init(&H, 1);
-  printf("@ Branch and Bound\n# initial incumbent = %f\n", incumbent);
+  printf("\n@ Branch and Bound\n# initial incumbent = %f\n", incumbent);
   solve_tsp(&G, &H, &incumbent, 0); 
 
   egraph EG1;
@@ -72,5 +78,5 @@ void main(int argc, char** argv) {
   egraph_copy(&EG, &EG1);
   onetree_to_egraph(&H, &EG1);
   egraph_plot(&EG, &EG1);
-
+  
 }
