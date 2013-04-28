8e0c7784abd8cc5c90120f04e4e0a99bd49dbd2d
ludovico.minto@gmail.com <ludovico.minto@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-04-28 19:53:24 +0000

    /test/TEST_tsp_v1b -> new functions to apply-check-propagate constraints

---
+++ test/TEST_tsp_v1b/constants.h
@@ -0,0 +1,26 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+//#define EPSILON 0.0
+#define EPSILON 0.0000001
+
+#define BIG 1000.0
+#define SMALL -1000.0
+
+#define Y 1
+#define N 0
+
+#define SUCCESS 1
+#define FAILURE 0
+
+#define MAX_NUM_ITERATIONS 30000
+#define MAX_NUM_ITERATIONS_NO_IMPROV 50
+#define ALPHA 2.0
+#define ALPHA_HALVING_TIME 10
+
+#define X_MIN -0.010
+#define X_MAX 1.010
+#define Y_MIN -0.010
+#define Y_MAX 1.010
+
+#endif
