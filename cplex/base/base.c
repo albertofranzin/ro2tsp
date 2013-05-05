10ddf683300fa38b0f677407ca889d45c08d165a
alberto.franzin@gmail.com <alberto.franzin@gmail.com@615c5c71-a7ff-86d5-8cea-1ab877cf2a19>
2013-05-05 12:42:57 +0000

    commit di salvataggio prima di metter mano a cplex

--- cplex/base/base.c
+++ cplex/base/base.c
@@ -10,7 +10,7 @@
  * return : pointer parameters object
  */
 parameters *base_problem_setup(int argc, char **argv) {
-  int        i;
+  int         i;
   char       *opt;
   parameters *pars = getParameters();
 
@@ -18,6 +18,12 @@ parameters *base_problem_setup(int argc, char **argv) {
   for (i = 1; i < argc; i++) {
 
     opt = argv[i];
+
+    if (strcmp(opt, "-h") == 0 || strcmp(opt, "--help") == 0) {
+      // print help and exit
+      print_helper_menu();
+      exit(0);
+    }
 
     if (strcmp(opt, "-n") == 0 || strcmp(opt, "--number") == 0)
       pars->number_of_nodes = atoi(argv[++i]);
@@ -32,16 +38,31 @@ parameters *base_problem_setup(int argc, char **argv) {
       pars->tsp_file = argv[++i];
     }
 
-    if (strcmp(opt, "-h") == 0 || strcmp(opt, "--heur") == 0) {
+    if (strcmp(opt, "--heur") == 0) {
+      if (strcmp(argv[i+1], "NEAREST_NEIGHBOUR") == 0 ||
+          strcmp(argv[i+1], "nn") == 0                  ) {
+        pars->heuristic_algo = NEAREST_NEIGHBOUR;
+      } else if (strcmp(argv[i+1], "NEAREST_NEIGHBOUR_2_OPT") == 0 ||
+                 strcmp(argv[i+1], "nn2opt") == 0                  ||
+                 strcmp(argv[i+1], "2opt") == 0                      ) {
+        pars->heuristic_algo = NEAREST_NEIGHBOUR_2_OPT;
+      } else if (strcmp(argv[i+1], "RANDOM_CYCLE") == 0 ||
+                 strcmp(argv[i+1], "rc") == 0             ) {
+        pars->heuristic_algo = RANDOM_CYCLE;
+      }
+      i++;
     }
 
     if (strcmp(opt, "--solver") == 0) {
-    }
-
-    if (strcmp(opt, "-h") == 0 || strcmp(opt, "--help") == 0) {
-      // print help and exit
-      print_helper_menu();
-      return 0;
+      if (strcmp(argv[i+1], "BRANCH_AND_BOUND") == 0 ||
+          strcmp(argv[i+1], "branch_and_bound") == 0 ||
+          strcmp(argv[i+1], "bb") == 0                 ) {
+        pars->solver = BRANCH_AND_BOUND;
+      } else if (strcmp(argv[i+1], "CPLEX") == 0 ||
+                 strcmp(argv[i+1], "cplex") == 0   ) {
+        pars->solver = CPLEX;
+      }
+      i++;
     }
 
   }
@@ -65,7 +86,7 @@ void print_helper_menu() {
   printf("\n");
   printf("  TSP solver for the course of Ricerca Operativa 2\n");
   printf("  a.y. 2012/13, taught by prof. M. Fischetti\n\n");
-  printf("  authors: Alberto Franzin\n         Ludovico Minto\n\n");
+  printf("  authors: Alberto Franzin\n           Ludovico Minto\n\n");
   printf("  Helper menu\n");
   printf("  This software can solve two kind on TSP instances:\n");
   printf("  - instances in TSPLIB format;\n");
@@ -79,9 +100,18 @@ void print_helper_menu() {
   printf("                      if x < 0, then a pseudorandom seed will be used.\n");
   printf("  -n [--number] x : x (positive integer) is the number of nodes to be generated\n");
   printf("                    in the random instance.\n");
-  printf("  -f [--file]   x : x (valid file/path) is the TSPLIB-formatted file of the instance.\n");
+  printf("  -f [--file] x   : x (valid file/path) is the TSPLIB-formatted file of the instance.\n");
   printf("                    If this option is chosen, then the previous ones\n");
   printf("                    will be ignored, if present.\n");
+  printf("  --solver x      : choose the solver method for the problem\n");
+  printf("                    x can be:\n");
+  printf("                    - BRANCH_AND_BOUND / branch_and_bound / bb\n");
+  printf("                    - CPLEX / cplex\n");
+  printf("  --heur x        : choose the heuristic algorithm for an upper bound\n");
+  printf("                    x can be:\n");
+  printf("                    - NEAREST_NEIGHBOUR / nn\n");
+  printf("                    - NEAREST_NEIGHBOUR_2_OPT / nn2opt / 2opt\n");
+  printf("                    - RANDOM_CYCLE / rc\n");
   printf("  -h [--help]     : printf this menu and exit.\n");
   printf("\n\n");
 }
--- cplex/test_cplex.c
+++ cplex/test_cplex.c
@@ -21,7 +21,9 @@ int main (int argc, char *argv[]) {
   if (pars->tsp_file_option == TRUE) {
     read_tsp_from_file(&EG, pars);
     egraph_to_graph(&EG, &G);
-    printf("@ Euclidean TSP\n# tsplib instance\n# number of nodes = %d\n# tsplib file = %s\n\n", pars->number_of_nodes, pars->tsp_file);
+    printf("@ Euclidean TSP\n# tsplib instance\n");
+    printf("# number of nodes = %d\n# tsplib file = %s\n\n",
+                pars->number_of_nodes, pars->tsp_file);
   }
   else if (pars->random_instance_option == TRUE) {
     egraph_random(&EG);
@@ -36,11 +38,8 @@ int main (int argc, char *argv[]) {
   printf("cycle created\n");
 
   double heuristic_upper_bound;
-  heuristic_upper_bound = compute_upper_bound(&G, &C, NEAREST_NEIGHBOUR);
+  heuristic_upper_bound = compute_upper_bound(&G, &C, pars->heuristic_algo);
   printf("@ Nearest Neighbour Heuristic\n# upper bound = %f\n", heuristic_upper_bound);
-
-  /** /heuristic_upper_bound = heur2opt(&G, &C, heuristic_upper_bound);
-  printf("@ 2-opt\nupper bound = %f\n", heuristic_upper_bound);/ **/
 
   /////////////////////////////////////////////////////////////////
   //
@@ -51,7 +50,46 @@ int main (int argc, char *argv[]) {
 
   double incumbent;
 
-  cplex_solve_problem(&G, &EG, &incumbent);
+  switch(pars->solver) {
+
+    case BRANCH_AND_BOUND :
+        incumbent = heuristic_upper_bound + EPSILON;
+        printf("heuristic_upper_bound = %.30f,\n", heuristic_upper_bound);
+        printf(")           incumbent = %.30f\n", incumbent);
+        onetree H;
+        onetree_init(&H, 1);
+
+        onetree ONE_TREE;
+        onetree_init(&ONE_TREE, 1);
+
+        double z;
+
+        z = compute_lagrange(&G, &ONE_TREE, incumbent);
+
+        if (is_cycle(&ONE_TREE) == 1) {
+          printf("lagrangiano FTW!\n");
+          egraph EG1;
+          egraph_init(&EG1, 1);
+          egraph_copy(&EG, &EG1);
+          onetree_to_egraph(&ONE_TREE, &EG1);
+          egraph_plot(&EG, &EG1);
+        } else {
+          printf("\n@ Branch and Bound\n# initial incumbent = %f, lower bound = %f\n",
+            incumbent, z);
+          solve_tsp(&G, &H, &incumbent, &ONE_TREE, z, 0);
+          egraph EG1;
+          egraph_init(&EG1, 1);
+          egraph_copy(&EG, &EG1);
+          onetree_to_egraph(&H, &EG1);
+          egraph_plot(&EG, &EG1);
+        }
+        break;
+
+    case CPLEX :
+        cplex_solve_problem(&G, &EG, &incumbent);
+        break;
+
+  }
 
   printf("final solution has cost %f\n", incumbent);
 
