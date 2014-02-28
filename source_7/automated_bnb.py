36ec74c90002d061aceec6387a26932b4f9685fc
Alberto Franzin <albertofranzin@users.noreply.github.com>
2014-02-28 13:21:15 +0000

    backup di alcuni risultati

--- source_7/automated_bnb.py
+++ source_7/automated_bnb.py
@@ -19,7 +19,7 @@ for i in output:
     result_subp = subprocess.Popen(["./test", "--file", i], stdout=subprocess.PIPE)#, shell=True)
     (results, err) = result_subp.communicate()
     # print results, err
-    outfilename = "bbres/" + instance + ".txt"
+    outfilename = "cplexcallbres/" + instance + ".txt"
     with open(outfilename, "w") as outfile:
         outfile.write("{}".format(results))
     
